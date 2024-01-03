ccf872a10d2b6e0072691f46149654b61e00e342
hyein7447 <hyein7447@gmail.com>
2024-01-03 09:39:10 +0900

    과제

---
+++ script/dom_task.js
@@ -0,0 +1,10 @@
+const userHeight = document.querySelector('#height')
+const userWeight = document.querySelector('#weight')
+const goals = document.querySelector('#goals')
+const result = document.querySelector('#result')
+console.log(userHeight, userWeight, goals, result)
+
+result.addEventListener('click',function(){
+    // console.log('.')
+    normal_w = (userHeight.value-100)
+})
